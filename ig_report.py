f6ac0fd2f0a5d4ed81d786d3381271100228df06
Srijal62044 <165928467+Srijal62044@users.noreply.github.com>
2026-01-13 13:25:06 +0530

    Update ig_report.py

--- ig_report.py
+++ ig_report.py
@@ -1,10 +1,9 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+
+
 
 
 
@@ -17,15 +16,16 @@ num_reports_input = input("Enter the number of reports: ")
 num_reports = int(num_reports_input)  # Convert to integer
 
 # Set up the webdriver (make sure you have ChromeDriver installed and in your PATH)
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--disable-gpu")
+options = Options()
+options.add_argument("--headless")
 
-service = Service("/data/data/com.termux/files/usr/bin/chromedriver")
+service = Service("/data/data/com.termux/files/usr/bin/geckodriver")
 
-driver = webdriver.Chrome(service=service, options=chrome_options) # Use webdriver.Chrome() for Chrome, or adjust as needed
+driver = webdriver.Firefox(service=service, options=options)
+
+
+
+ # Use webdriver.Chrome() for Chrome, or adjust as needed
 
 try:
     # Step 1: Go to Instagram login page
